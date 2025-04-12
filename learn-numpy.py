112943c78f5a1d510053d80d68b14d210ab75ebd
binkcoding <malachysutton@gmail.com>
2025-04-12 16:11:39 +1000

    learning NP and printing all outputs

--- learn-numpy.py
+++ learn-numpy.py
@@ -1,11 +1,21 @@
 import numpy as np
-
+from numpy import arange
 #One dimensional array
 sample_list = [1, 2, 3]
 
 #Two dimensional array
 my_matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-#Display both arrays
+#Arrange function
+np.arange(1, 11, 2)
+np.zeros(4)
+np.ones(4)
+
+#Display all outputs
+print(np.ones(4))
+print(np.zeros(4))
+print(np.arange(1, 11, 2))
 print(np.array(my_matrix))
 print(np.array(sample_list))
+
+
