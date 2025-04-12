f367932303fd475d4fcc106539fde8447777229f
binkcoding <malachysutton@gmail.com>
2025-04-12 16:05:10 +1000

    Learning basic one dimensional and two dimensional arrays

--- learn-numpy.py
+++ learn-numpy.py
@@ -1,1 +1,11 @@
-#Testing
+import numpy as np
+
+#One dimensional array
+sample_list = [1, 2, 3]
+
+#Two dimensional array
+my_matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+#Display both arrays
+print(np.array(my_matrix))
+print(np.array(sample_list))
